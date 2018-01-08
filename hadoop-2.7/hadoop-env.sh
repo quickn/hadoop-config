3b45fccdc95989a587a2ecc1927e13714de87a65
liuyun <260418992@qq.com>
2018-01-08 16:41:42 +0800

    通过mvn profile 方式管理文件

--- hadoop-2.7/hadoop-env.sh
+++ hadoop-2.7/hadoop-env.sh
@@ -96,4 +96,4 @@ export HADOOP_SECURE_DN_PID_DIR=${HADOOP_PID_DIR}
 
 # A string representing this instance of hadoop. $USER by default.
 export HADOOP_IDENT_STRING=$USER
-export JAVA_HOME=/usr/local/src/java
+#export JAVA_HOME=${JAVA_HOME}
