555000b7a219a8b2cd57f6873d824b0a7bf0bf7b
liuyun <260418992@qq.com>
2018-01-09 19:36:46 +0800

    修改mvn

--- copy-conf.sh
+++ copy-conf.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 cp -rf ~/hadoop-config/apache-maven-3.3/* $LOCAL/${mvn.version}/conf/
-cp -rf ~/hadoop-config/target/classes/hadoop-2.7/* $src/${hadoop.version}/etc/hadoop/
-cp -rf ~/hadoop-config/target/classes/spark-2.2/* $src/${spark.version}/conf/
+cp -rf ~/hadoop-config/target/classes/hadoop-2.7/* $SRC/${hadoop.version}/etc/hadoop/
+cp -rf ~/hadoop-config/target/classes/spark-2.2/* $SRC/${spark.version}/conf/
 
-cp -rf ~/hadoop-config/target/classes/zookeeper-3.4/* $src/${zookeeper.version}/conf/
-cp -rf ~/hadoop-config/target/classes/kafka_2.1/* $src/${kafka.version}/config/
-cp -rf ~/hadoop-config/target/classes/jstorm-2.1/* $src/${jstorm.version}/conf/
-cp -rf ~/hadoop-config/target/classes/flume-1.8/* $src/${flume.version}/conf/
+cp -rf ~/hadoop-config/target/classes/zookeeper-3.4/* $SRC/${zookeeper.version}/conf/
+cp -rf ~/hadoop-config/target/classes/kafka_2.1/* $SRC/${kafka.version}/config/
+cp -rf ~/hadoop-config/target/classes/jstorm-2.1/* $SRC/${jstorm.version}/conf/
+cp -rf ~/hadoop-config/target/classes/flume-1.8/* $SRC/${flume.version}/conf/
 
