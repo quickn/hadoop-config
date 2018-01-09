e48a54c3b55cedcd0fc152bf972c28559d94ab0d
liuyun <260418992@qq.com>
2018-01-09 10:27:36 +0800

    添加zookeeper下載地址

--- copy-conf.sh
+++ copy-conf.sh
@@ -4,7 +4,7 @@ cp -rf ~/hadoop-config/target/classes/hadoop-2.7/* $src/hadoop-2.7.5/etc/hadoop/
 cp -rf ~/hadoop-config/target/classes/spark-2.2/* $src/spark-2.2.1-bin-hadoop2.7/conf/
 
 cp -rf ~/hadoop-config/target/classes/zookeeper-3.4/* $src/zookeeper-3.4.10/conf/
-cp -rf ~/hadoop-config/target/classes/kafka_2.1/* $src/kafka_2.12/config/
+cp -rf ~/hadoop-config/target/classes/kafka_2.1/* $src/kafka_2.12-1.0.0/config/
 cp -rf ~/hadoop-config/target/classes/jstorm-2.1/* $src/jstorm-2.1.1/conf/
 cp -rf ~/hadoop-config/target/classes/flume-1.8/* $src/flume-1.8.0/conf/
 
